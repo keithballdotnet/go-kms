5985ae16f744bc260f97b257df29b62310b28696
Inflatablewoman <keith@keithball.net>
2015-02-15 19:55:17 +0100

    Adding boiler plate

--- kms/server.go
+++ kms/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/rcrowley/go-tigertonic"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -16,6 +17,12 @@ var (
 	SharedKey = ""
 )
 
+// Context information for Marshaled calls
+type Context struct {
+	UserAgent  string
+	RemoteAddr string
+}
+
 // Start a HTTP listener
 func StartListener() {
 
@@ -25,6 +32,30 @@ func StartListener() {
 	// Set-up API listeners
 	mux := tigertonic.NewTrieServeMux()
 	mux.Handle("GET", "/api/v1/go-kms", tigertonic.Timed(tigertonic.Marshaled(GetHello), "GetHelloHandler", nil))
+	mux.Handle("POST", "/api/v1/go-kms/generatedatakey", tigertonic.If(
+		func(r *http.Request) (http.Header, error) {
+			tigertonic.Context(r).(*Context).UserAgent = r.UserAgent()
+			tigertonic.Context(r).(*Context).RemoteAddr = RequestAddr(r)
+			return nil, nil
+		},
+		tigertonic.Marshaled(generateDataKeyHandler),
+	))
+	mux.Handle("POST", "/api/v1/go-kms/decrypt", tigertonic.If(
+		func(r *http.Request) (http.Header, error) {
+			tigertonic.Context(r).(*Context).UserAgent = r.UserAgent()
+			tigertonic.Context(r).(*Context).RemoteAddr = RequestAddr(r)
+			return nil, nil
+		},
+		tigertonic.Marshaled(decryptHandler),
+	))
+	mux.Handle("POST", "/api/v1/go-kms/encrypt", tigertonic.If(
+		func(r *http.Request) (http.Header, error) {
+			tigertonic.Context(r).(*Context).UserAgent = r.UserAgent()
+			tigertonic.Context(r).(*Context).RemoteAddr = RequestAddr(r)
+			return nil, nil
+		},
+		tigertonic.Marshaled(encryptHandler),
+	))
 
 	// Log to Console
 	server := tigertonic.NewServer(fmt.Sprintf("%s:%s", Config["GOKMS_HOST"], Config["GOKMS_PORT"]), tigertonic.ApacheLogged(mux))
@@ -65,6 +96,32 @@ func SetupAuthenticationKey() {
 	SharedKey = string(bytes)
 }
 
+// GenerateDataKeyRequest
+type GenerateDataKeyRequest struct {
+	KeyID string
+}
+
+// GenerateDataKeyResponse
+type GenerateDataKeyResponse struct {
+	Plaintext      []byte
+	CiphertextBlob []byte
+}
+
+// generateDataKeyHandler will generate a new AES key for use by a client
+func generateDataKeyHandler(u *url.URL, h http.Header, dataKeyRequest *GenerateDataKeyRequest, c *Context) (int, http.Header, *GenerateDataKeyResponse, error) {
+	//var err error
+	//defer CatchPanic(&err, "GenerateDataKeyRequest")
+
+	log.Println("GenerateDataKeyRequest: Starting...")
+
+	// Create a new key
+	aesKey := GenerateAesSecret()
+
+	// Encrypt the key with the master key
+
+	return http.StatusOK, nil, &GenerateDataKeyResponse{Plaintext: aesKey}, nil
+}
+
 func GetHello(u *url.URL, h http.Header, _ interface{}) (int, http.Header, string, error) {
 	log.Println("Got GET hello request")
 
@@ -75,7 +132,7 @@ func GetHello(u *url.URL, h http.Header, _ interface{}) (int, http.Header, strin
 // AuthorizeRequest - Will check the request authorization
 func AuthorizeRequest(method string, u *url.URL, h http.Header) bool {
 
-	date := h.Get("x-blocker-date")
+	date := h.Get("x-kms-date")
 	resource := u.Path
 	authRequestKey := fmt.Sprintf("%s\n%s\n%s", method, date, resource)
 
@@ -89,4 +146,11 @@ func AuthorizeRequest(method string, u *url.URL, h http.Header) bool {
 
 	// Was the passed value the same as we expected?
 	return authorization == hmac
+}
+
+// Get the request address
+func RequestAddr(r *http.Request) string {
+	// Get the IP of the request
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return ip
 }
